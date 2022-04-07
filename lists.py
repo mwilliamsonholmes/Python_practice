1df81b509735a0ad29f701d8d04da6cfed67fd5c
Melissa <mawilliamson10@gmail.com>
2022-04-07 13:09:56 -0700

    access indices of array

---
+++ lists.py
@@ -0,0 +1,14 @@
+largestLandAnimals = [
+  "Elephant",
+  "Rhino",
+  "Hippo",
+  "Gaur",
+  "Giraffe",
+  "Walrus",
+  "Croc",
+  "Buffalo"
+]
+
+print(largestLandAnimals[3])
+print(largestLandAnimals[5])
+print(largestLandAnimals[6])
