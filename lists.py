4d3609d9f63ff7d1e082b803e6637830729b384e
Melissa <mawilliamson10@gmail.com>
2022-04-11 12:53:59 -0700

    list slicing

--- lists.py
+++ lists.py
@@ -12,3 +12,19 @@ largestLandAnimals = [
 print(largestLandAnimals[3])
 print(largestLandAnimals[5])
 print(largestLandAnimals[6])
+
+players = [
+  "Mia",
+  "Retta",
+  "Augustine",
+  "Jin",
+  "Waylon",
+  "Jamel",
+  "Alphonso",
+  "Sage"
+]
+# print(players[0:3])
+print(players[3:5])
+print(players[:3])
+print(players[-3])
+print(players[-1])
